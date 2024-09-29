b6241ddbb6b3abf5fc0c72de64c549482e451f43
PlotoZypresse <janlucca1401@gmail.com>
2024-09-29 21:41:32 +0200

    added clan info command and basic functions for it

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 import os
 import discord
 from discord.ext import commands
-import cr_api
+import playerInfo
+import clanInfo
 
 
 load_dotenv()
@@ -28,16 +29,16 @@ async def ping(ctx):
 
 @bot.command()
 async def name(ctx):
-    await ctx.send(cr_api.printName('data.json'))
+    await ctx.send(playerInfo.printName('data.json'))
 
 @bot.command()
 async def tag(ctx):
-    await ctx.send(cr_api.printTag('data.json'))
+    await ctx.send(playerInfo.printTag('data.json'))
 
 @bot.command()
-async def getPlayerInfo(ctx, player_tag: str):
+async def PlayerInfo(ctx, player_tag: str):
     filename = f"data_{player_tag}.json"
-    player_info = cr_api.PlayerInfo(player_tag, filename)
+    player_info = playerInfo.PlayerInfo(player_tag, filename)
 
     try:
         player_info.getPlayerData()
@@ -45,10 +46,27 @@ async def getPlayerInfo(ctx, player_tag: str):
         name = player_info.getName()
         winRate = player_info.playerWinRate()
         clan = player_info.clanInfo()
+
         await ctx.send(f'Player Tag: {tag} \nPlayer Name: {name} \n{winRate} \nPlayer Clan: {clan}')
     except Exception as e:
         await ctx.send(f'An error occurred: {str(e)}')
     finally:
         player_info.deleteFile()
+
+@bot.command()
+async def ClanInfo(ctx, clan_tag: str):
+    filename = f"data{clan_tag}.json"
+    clan_info = clanInfo.clanInfo(clan_tag, filename)
+
+    try:
+        clan_info.getClanData()
+        tag = clan_info.getTag()
+        name = clan_info.getName()
+
+        await ctx.send(f'Clan Tag: {tag} \nClan Name: {name}')
+    except Exception as e:
+        await ctx.send(f'An error occured: {str(e)}')
+    finally:
+        clan_info.deleteFile()
 
 bot.run(os.getenv('DISCORD_API_KEY'))
---
+++ clanInfo.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+import requests
+import os
+import json
+
+class clanInfo:
+    def __init__(self, clan_tag, filename):
+        self.clan_tag = clan_tag
+        self.filename = filename or f"data_{clan_tag}.json"
+
+    def getClanData(self):
+        load_dotenv()
+        api_key = os.getenv('CLASH_API_KEY')
+
+        # error hanfling for api key
+        if not api_key:
+            raise ValueError("API key is not set in environment variables.")
+
+        clan_tag = self.clan_tag
+        url = f'https://api.clashroyale.com/v1/clans/{clan_tag.replace("#", "%23")}'
+
+        headers = {
+            'Authorization': f'Bearer {api_key}'
+        }
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            with open(self.filename, 'w') as json_file:
+                json.dump(response.json(), json_file, indent=2)
+
+        else:
+            print(f"Error: {response.status_code}")
+        
+    def _getData(self):
+        with open(self.filename, 'r') as file:
+            return json.load(file)
+        
+    def getTag(self):
+        data = self._getData()
+        return data.get('tag', 'Tag not found')
+
+    def getName(self):
+        data = self._getData()
+        return data.get('name', 'Name not found')
+    
+    def deleteFile(self):
+        try:
+            os.remove(self.filename)
+            print(f"{self.filename} has been deleted.")
+        except OSError as e:
+            print(f"Error deleting file: {e}")
+
